ceecd903e7888b18b447b21d31990ac326fbf98c
priyansudash1999 <1999priyansudash@gmail.com>
2026-02-01 00:16:18 +0530

    feat: add sales by category pie chart component

--- src/components/ChatSection.jsx
+++ src/components/ChatSection.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import RvenueChart from "./RvenueChart";
+import SalesChart from "./SalesChart";
 
 const ChatSection = () => {
   return (
     <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
       <div className="xl:col-span-2">
         <RvenueChart />
+      </div>
+      <div className="space-y-6">
+        <SalesChart />
       </div>
     </div>
   );
