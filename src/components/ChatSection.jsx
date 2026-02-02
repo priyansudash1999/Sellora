9e3645f6054ff377cf5745fa3f02220f7401994f
priyansudash1999 <1999priyansudash@gmail.com>
2026-02-02 20:47:06 +0530

    fix: ChatSection media query

--- src/components/ChatSection.jsx
+++ src/components/ChatSection.jsx
@@ -4,11 +4,14 @@ import SalesChart from "./SalesChart";
 
 const ChatSection = () => {
   return (
-    <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
-      <div className="xl:col-span-2">
+    <div className="grid grid-cols-1 gap-4 sm:gap-6 xl:grid-cols-3">
+      {/* Revenue Chart */}
+      <div className="xl:col-span-2 order-1">
         <RvenueChart />
       </div>
-      <div className="space-y-6">
+
+      {/* Sales Chart */}
+      <div className="order-2">
         <SalesChart />
       </div>
     </div>
